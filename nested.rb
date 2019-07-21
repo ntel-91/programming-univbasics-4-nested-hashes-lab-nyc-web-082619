70ac5bfe36b2892a51d566ac7aa72e60e3ed3e35
Nick Telenson <ntelenson@gmail.com>
2019-07-21 21:55:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,7 +116,7 @@ def changing_alan
      alans_new_info = "GUI"
      
     programmer_hash[:alan_kay][:known_for] = "GUI"
-    #programmer_hash[:alan_kay][:known_for]
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
