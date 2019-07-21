1d7cd5a74d35d7a6cc86733ff8359c61d7da2efa
Nick Telenson <ntelenson@gmail.com>
2019-07-21 22:02:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-    programmer_hash
+  programmer_hash
     
 
 end
@@ -133,8 +133,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
-
+  
+  programmer_hash
+  
 end
